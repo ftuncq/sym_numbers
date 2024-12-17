435182ae8cca771a8a1c533add1802279f4500ab
ftuncq <ftuncq@gmail.com>
2024-12-17 12:17:05 +0100

    Mise en place appli

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
 import './bootstrap.js';
+import { Application } from "@hotwired/stimulus";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +7,26 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+window.Stimulus = Application.start();
+
+// Close alert message after 5 secondes
+const closeAlertMessage = () => {
+    const alert = document.querySelector('.alert');
+    if (alert) {
+        setTimeout(() => {
+            alert.classList.add('fade-out'); // Ajoutez une classe CSS pour transition
+            setTimeout(() => alert.remove(), 1000); // Retire l'alerte après la transition
+        }, 4000);
+    }
+}
+
+const initPage = () => {
+    closeAlertMessage();
+};
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+document.addEventListener('load', initPage);
+document.addEventListener('turbo:load', initPage);
